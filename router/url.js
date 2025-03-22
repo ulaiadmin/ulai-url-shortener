3fa34cf54ed82fbd82fe64c7b4322c7642185c04
KartikUlai <kartik@ulai.in>
2025-03-22 23:52:54 +0530

    feat-added

--- router/url.js
+++ router/url.js
@@ -1,10 +1,9 @@
 import express from "express";
 import {
-    handleGenerateNewShortURL,
-    handleGetAnalytics,
+  handleGenerateNewShortURL,
+  handleGetAnalytics,
 } from "../controllers/shortUrl.js";
 import short_url_trackers from "../models/shortUrlTracker.js";
-
 
 const router = express.Router();
 
@@ -12,19 +11,23 @@ router.post("/create", handleGenerateNewShortURL);
 router.get("/analytics/:userId", handleGetAnalytics);
 router.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
-  const entry = await short_url_trackers.findOneAndUpdate(
-    {
-      shortId,
-    },
-    {
-      $push: {
-        visitHistory: {
-          timestamp: new Date(),
-        },
+  if (shortId.includes("https://")) {
+    res.redirect(shortId);
+  } else {
+    const entry = await short_url_trackers.findOneAndUpdate(
+      {
+        shortId,
       },
-    }
-  );
-  res.redirect(entry.redirectURL);
+      {
+        $push: {
+          visitHistory: {
+            timestamp: new Date(),
+          },
+        },
+      }
+    );
+    res.redirect(entry.redirectURL);
+  }
 });
 
 export default router;
